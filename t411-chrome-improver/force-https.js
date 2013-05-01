367f22ad340aec2ad2b3ba4f7eab06554c5ef07d
daniel.liabeuf <daniel.liabeuf@gmail.com>
2013-05-01 14:57:11 +0200

    Ajout de fonctionnalités. Lorsque HTTPS est utilisé, t411-improver cherche les urls dont la destination n'est pas sécurisé. Il corrige ça quand la destination est www, wiki ou forum.t411.me

---
+++ t411-chrome-improver/force-https.js
@@ -0,0 +1,15 @@
+(function() {
+
+var httpsableDomains = new RegExp("http://((www|wiki|forum).)?t411.me/");
+var unsecuredTarget = document.querySelectorAll('a[href^="http://"]');
+var qtCorrectedUrls=0;
+for (var i=0; i< unsecuredTarget.length; i++) {
+	var aLink=unsecuredTarget[i];
+	if (httpsableDomains.exec(aLink.href)) {
+		aLink.href = "https"+aLink.href.substring(4);
+		qtCorrectedUrls++;
+	}
+}
+console.log("T411-Improver a corrigé "+qtCorrectedUrls+" urls non-sécurisées sur "+unsecuredTarget.length);
+
+})();
