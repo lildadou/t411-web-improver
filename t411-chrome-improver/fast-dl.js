bb0006e3be9508ea48c65ab23b9ec779c6683a8c
manandre <manandre94@gmail.com>
2016-11-25 22:53:25 +0100

    Passage à t411.li

--- t411-chrome-improver/fast-dl.js
+++ t411-chrome-improver/fast-dl.js
@@ -6,14 +6,14 @@ getSettings(function(settings) {
 	var dwnIco = chrome.extension.getURL("Download-64.png");
 	for (var i=0; i<allTorrents.length; i++) {
 		var nfoImg = new Image();
-		nfoImg.src = "http://www.t411.ch/images/icon_nfo.gif";
+		nfoImg.src = "http://www.t411.li/images/icon_nfo.gif";
 		var nfoLink = allTorrents[i].querySelector("a.nfo");
 		nfoLink.classList.remove("nfo");
 		nfoLink.appendChild(nfoImg);
 		
 		var tId = /\d+$/.exec(nfoLink.href)[0];
 		var dlLink = document.createElement("a");
-		dlLink.href = "http://www.t411.ch/torrents/download/?id="+tId;
+		dlLink.href = "http://www.t411.li/torrents/download/?id="+tId;
 		dlLink.setAttribute("alt","Lien de téléchargement direct");
 		var dlImg = new Image();
 		dlImg.src = dwnIco;
